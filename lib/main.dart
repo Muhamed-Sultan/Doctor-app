83336b379c4671a3b3d12639274a1592a5af7340
Muhamed Salah Sultan <mohsala120@gmail.com>
2026-09-11 18:56:28 +0300

    doctor app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-// import 'package:doctor_app/screens/cover_screen.dart';
-// import 'package:doctor_app/screens/login_screen.dart';
-// import 'package:doctor_app/screens/register_screen.dart';
+import 'package:doctor_app/screens/cover_screen.dart';
+import 'package:doctor_app/screens/login_screen.dart';
+import 'package:doctor_app/screens/register_screen.dart';
 import 'package:doctor_app/screens/doctor_details_screen.dart';
-// import 'package:doctor_app/screens/home_screen.dart';
+import 'package:doctor_app/screens/home_screen.dart';
 import 'package:doctor_app/widget/custom_bottom_nev_bar.dart';
-// import 'package:doctor_app/screens/payment_successfully.dart';
+import 'package:doctor_app/screens/payment_successfully.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,13 +13,13 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: PageView(
         children: [
-          // CoverScreen(),
-          // LoginScreen(),
-          // RegisterScreen(),
+          CoverScreen(),
+          LoginScreen(),
+          RegisterScreen(),
           ButtomNevBarScreen(),
           // HomeScreen(),
           DoctorDetailsScreen(),
-          // PaymentSuccessfully(),
+          PaymentSuccessfully(),
         ],
       ),
     ),
